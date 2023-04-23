5823422a366fb1eccc577618f607121cf9ab27cc
Hyeonjung Ko <hjko1203@gmail.com>
2023-04-23 14:09:25 -0400

    added tests for accounts

---
+++ util/random.go
@@ -0,0 +1,47 @@
+package util
+
+import (
+	"math/rand"
+	"strings"
+	"time"
+)
+
+const alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+// RandInt generates a random value between the provided min and max values
+func RandInt(min, max int64) int64 {
+	return min + rand.Int63n(max-min+1)
+}
+
+// RandString generates a random alphabetical string of length n
+func RandString(n int) string {
+	var sb strings.Builder
+	k := len(alphabet)
+
+	for i := 0; i < n; i++ {
+		c := alphabet[rand.Intn(k)]
+		sb.WriteByte(c)
+	}
+
+	return sb.String()
+}
+
+// RandOwner generates a random owner string
+func RandomOwner() string {
+	return RandString(6)
+}
+
+func RandomMoney() int64 {
+	return RandInt(0, 1000000)
+}
+
+func RandomCurrency() string {
+	currencies := []string{"EUR", "USD", "CAD", "KRW"}
+	n := len(currencies)
+
+	return currencies[rand.Intn(n)]
+}
