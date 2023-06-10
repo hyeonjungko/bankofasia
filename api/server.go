85841f6c3a8d4dd3128b77964e9865c4e2c527b6
Hyeonjung Ko <hjko1203@gmail.com>
2023-06-10 22:00:20 +0900

    created server, added POST & GET /accounts routes.

---
+++ api/server.go
@@ -0,0 +1,34 @@
+package api
+
+import (
+	"github.com/gin-gonic/gin"
+	db "github.com/hyeonjungko/bankofasia/db/sqlc"
+)
+
+// Server serves HTTP requests
+type Server struct {
+	store  *db.Store
+	router *gin.Engine
+}
+
+// NewServer creates new server and sets up routing
+func NewServer(store *db.Store) *Server {
+	server := &Server{store: store}
+	router := gin.Default()
+
+	router.POST("/accounts", server.createAccount)
+	router.GET("/accounts/:id", server.getAccount)
+	router.GET("/accounts", server.listAccount)
+
+	server.router = router
+	return server
+}
+
+// Start runs HTTP server on given address
+func (server *Server) Start(address string) error {
+	return server.router.Run(address)
+}
+
+func errorResponse(err error) gin.H {
+	return gin.H{"error": err.Error()}
+}
