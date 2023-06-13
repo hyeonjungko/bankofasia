8a7130e4653e906d2999a4109e525328264258eb
Hyeonjung Ko <hjko1203@gmail.com>
2023-06-13 18:26:57 +0900

    add test for /transfers. update README

--- README.md
+++ README.md
@@ -8,8 +8,14 @@ one bank, all assets.
 * created Makefile for docker container creation, db up/down migrations
 * added tests for DB transactions, preventing deadlocks
 * added Git workflow for tests for `main` branch
+* added server routes (+ tests) for `account` related functionality
+* added server routes for `transfer` related functionality
 
-## DB
+## External Packages
 * [`sqlc`](https://sqlc.dev/) to translate SQL into Go code
 * [`testify`](https://github.com/stretchr/testify) to ease test writing
 * [`migrate`](https://github.com/golang-migrate/migrate) to migrate DB versions up/down
+* [`viper`](https://github.com/spf13/viper) to read/set env & configuration variables
+
+## TODO Backlog
+* Add tests for all errors of `/transfers` to gain full coverage 
---
+++ api/transfer_test.go
@@ -0,0 +1,84 @@
+package api
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang/mock/gomock"
+	mockdb "github.com/hyeonjungko/bankofasia/db/mock"
+	db "github.com/hyeonjungko/bankofasia/db/sqlc"
+	"github.com/hyeonjungko/bankofasia/util"
+	"github.com/stretchr/testify/require"
+)
+
+func TestCreateTransferAPI(t *testing.T) {
+	amount := int64(10)
+	account1 := randomAccount()
+	account2 := randomAccount()
+
+	account1.Currency = util.USD
+	account2.Currency = util.USD
+
+	testCases := []struct {
+		name          string
+		body          gin.H
+		buildStubs    func(store *mockdb.MockStore)
+		checkResponse func(t *testing.T, recorder *httptest.ResponseRecorder)
+	}{
+		{
+			name: "OK",
+			body: gin.H{
+				"from_account_id": account1.ID,
+				"to_account_id":   account2.ID,
+				"amount":          amount,
+				"currency":        util.USD,
+			},
+			buildStubs: func(store *mockdb.MockStore) {
+				// add expections of GetAccount for account1 & 2
+				store.EXPECT().GetAccount(gomock.Any(), gomock.Eq(account1.ID)).Times(1).Return(account1, nil)
+				store.EXPECT().GetAccount(gomock.Any(), gomock.Eq(account2.ID)).Times(1).Return(account2, nil)
+
+				arg := db.TransferTxParams{
+					FromAccountID: account1.ID,
+					ToAccountID:   account2.ID,
+					Amount:        amount,
+				}
+				store.EXPECT().TransferTx(gomock.Any(), gomock.Eq(arg)).
+					Times(1)
+			},
+			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusOK, recorder.Code)
+			},
+		},
+	}
+
+	for i := range testCases {
+		tc := testCases[i]
+
+		t.Run(tc.name, func(t *testing.T) {
+			controller := gomock.NewController(t)
+			defer controller.Finish()
+
+			store := mockdb.NewMockStore(controller)
+			tc.buildStubs(store)
+
+			server := NewServer(store)
+			recorder := httptest.NewRecorder()
+
+			data, err := json.Marshal(tc.body)
+			require.NoError(t, err)
+
+			url := "/transfers"
+			req, err := http.NewRequest(http.MethodPost, url, bytes.NewReader(data))
+			require.NoError(t, err)
+
+			server.router.ServeHTTP(recorder, req)
+			tc.checkResponse(t, recorder)
+		})
+	}
+
+}
