befe30f8346ab975940447b5e20b30ee3b8c8a13
Hyeonjung Ko <hjko1203@gmail.com>
2023-09-24 14:48:21 +0900

    feature: perform db migrations within go instead of Docker

--- main.go
+++ main.go
@@ -9,6 +9,9 @@ import (
 	"net"
 	"net/http"
 
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"github.com/hyeonjungko/bankofasia/api"
 	db "github.com/hyeonjungko/bankofasia/db/sqlc"
@@ -40,12 +43,24 @@ func main() {
 		log.Fatal("cannot connect to db:", err)
 	}
 
+	runDBMigration(config.MigrationURL, config.DBSource)
+
 	store := db.NewStore(conn)
-
-	// runGinHttpServer(config, store)
-
 	go runGatewayServer(config, store) // run HTTP gateway server in a separate routine
 	runGrpcServer(config, store)
+}
+
+func runDBMigration(migrationURL string, dbSource string) {
+	migration, err := migrate.New(migrationURL, dbSource)
+	if err != nil {
+		log.Fatal("cannot create new migrate instance: ", err)
+	}
+
+	if err = migration.Up(); err != nil && err != migrate.ErrNoChange {
+		log.Fatal("failed to run migrate up: ", err)
+	}
+
+	log.Println("db migrated successfully")
 }
 
 func runGrpcServer(config util.Config, store db.Store) {
