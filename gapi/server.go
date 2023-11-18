d1477202bccfa60b41cbf9c3510453cf61ecee72
Hyeonjung Ko <hjko1203@gmail.com>
2023-11-18 15:36:07 +0900

    feat: async task: send verification email on user sign up

--- gapi/server.go
+++ gapi/server.go
@@ -7,27 +7,30 @@ import (
 	"github.com/hyeonjungko/bankofasia/pb"
 	"github.com/hyeonjungko/bankofasia/token"
 	"github.com/hyeonjungko/bankofasia/util"
+	"github.com/hyeonjungko/bankofasia/worker"
 )
 
 // Server serves gRPC requests
 type Server struct {
 	pb.UnimplementedBankOfAsiaServer
-	config     util.Config
-	store      db.Store
-	tokenMaker token.Maker
+	config          util.Config
+	store           db.Store
+	tokenMaker      token.Maker
+	taskDistributor worker.TaskDistributor
 }
 
 // NewServer creates new gRPC server
-func NewServer(config util.Config, store db.Store) (*Server, error) {
+func NewServer(config util.Config, store db.Store, taskDistributor worker.TaskDistributor) (*Server, error) {
 	tokenMaker, err := token.NewPasetoMaker(config.TokenSymmetricKey)
 	if err != nil {
 		return nil, fmt.Errorf("cannot create token maker: %w", err)
 	}
 
 	server := &Server{
-		config:     config,
-		store:      store,
-		tokenMaker: tokenMaker,
+		config:          config,
+		store:           store,
+		tokenMaker:      tokenMaker,
+		taskDistributor: taskDistributor,
 	}
 
 	return server, nil
